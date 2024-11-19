28bd6d06eaf30b038fe5bfe08183e9021df6133e
Lucas Leao <lucasleao@gmail.com>
2024-11-19 18:04:13 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,9 @@ public class Program {
 		Department dep = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
 		list.stream().forEach(System.out::println);
+		System.out.println("\n=== TEST 3: seller findAll ===");
+		list = sellerDao.findAll();
+		list.stream().forEach(System.out::println);
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -133,8 +133,33 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			List<Seller> sellers = new ArrayList<>();
+			conn = DB.getConnection();
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentId = department.Id "
+					+"ORDER BY Name");
+			rs = st.executeQuery();
+			Map<Integer, Department> map = new HashMap<>();
+			while(rs.next() != false) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				Seller seller = instantiateSeller(rs, dep);
+				sellers.add(seller);
+			}
+			return sellers;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+
 	}
 
 }
